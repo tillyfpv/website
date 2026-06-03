1593062650436c641a7580211fbba6a07c931fa0
tillyfpv <weickm@email.de>
2026-06-03 17:06:09 +0200

    Update gallery.js

--- data/gallery.js
+++ data/gallery.js
@@ -1,1 +1,14 @@
-
+const galleryData = [
+    {
+        title: "Golden Hour Session",
+        image: "asset/hero1.jpg"
+    },
+    {
+        title: "FPV Moment",
+        image: "asset/hero1.jpg"
+    },
+    {
+        title: "Sunset Flight",
+        image: "asset/hero1.jpg"
+    }
+];
