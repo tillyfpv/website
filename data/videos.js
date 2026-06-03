e6a93dace7f11c8264fd159c3f1e915e2472aa40
tillyfpv <weickm@email.de>
2026-06-03 17:05:48 +0200

    Update videos.js

--- data/videos.js
+++ data/videos.js
@@ -1,1 +1,19 @@
+const videosData = {
+    en: [
+        {
+            title: "Pure Emotion",
+            category: "FPV Freestyle",
+            link: "https://youtube.com/shorts/myYRoYWM76E",
+            thumbnail: "asset/hero1.jpg"
+        }
+    ],
 
+    de: [
+        {
+            title: "Pure Emotion",
+            category: "FPV Freestyle",
+            link: "https://youtube.com/shorts/myYRoYWM76E",
+            thumbnail: "asset/hero1.jpg"
+        }
+    ]
+};
