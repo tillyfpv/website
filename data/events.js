fb9fdf6046521755e18d433e49b9143d22c76c2d
tillyfpv <weickm@email.de>
2026-06-03 17:05:19 +0200

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,1 +1,21 @@
+const eventsData = {
+    en: [
+        {
+            date: "20.06.26",
+            title: "Oldtimer Treffen",
+            location: "Kade",
+            status: "fix",
+            mapsLink: "https://maps.app.goo.gl/SEyyg4e7LS3Mj47PA"
+        }
+    ],
 
+    de: [
+        {
+            date: "20.06.26",
+            title: "Oldtimer Treffen",
+            location: "Kade",
+            status: "fix",
+            mapsLink: "https://maps.app.goo.gl/SEyyg4e7LS3Mj47PA"
+        }
+    ]
+};
