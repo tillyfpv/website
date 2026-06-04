557311660fc1f3c71895d678091e60897375b266
tillyfpv <weickm@email.de>
2026-06-04 14:04:38 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,1 +1,16 @@
+const settings = {
+    social: {
+        youtube: "https://youtube.com/@tilly_fpv",
+        instagram: "https://www.instagram.com/tilly_fpv/",
+        tiktok: "https://www.tiktok.com/@tilly79_mw"
+    },
 
+    contact: {
+        email: "kontakt@tillyfpv.de"
+    },
+
+    booking: {
+        enabled: false,
+        buttonLink: "mailto:kontakt@tillyfpv.de?subject=Booking%20Request%20Tilly%20FPV"
+    }
+};
