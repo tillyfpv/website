ee31ecdd8f0e4fc51fb754f6ba3e6cddc3328c49
tillyfpv <weickm@email.de>
2026-06-04 14:06:53 +0200

    Update gallery.js

--- data/gallery.js
+++ data/gallery.js
@@ -8,31 +8,7 @@ const galleryData = [
         image: "asset/hero1.jpg"
     },
     {
-        title: "FPV Moment",
-        image: "asset/hero1.jpg"
-    },
-    {
-        title: "FPV Moment",
-        image: "asset/hero1.jpg"
-    },
-    {
-        title: "FPV Moment",
-        image: "asset/hero1.jpg"
-    },
-    {
-        title: "FPV Moment",
-        image: "asset/hero1.jpg"
-    },
-    {
-        title: "FPV Moment",
-        image: "asset/hero1.jpg"
-    },
-    {
-        title: "FPV Moment",
-        image: "asset/hero1.jpg"
-    },
-    {
         title: "Sunset Flight",
         image: "asset/hero1.jpg"
-     }        
+    }
 ];
