c4154fc9fbea977a83cee236129f5a8c8c242461
tillyfpv <weickm@email.de>
2026-06-09 09:50:58 +0200

    Update gallery.js

--- data/gallery.js
+++ data/gallery.js
@@ -1,14 +1,14 @@
 const galleryData = [
     {
-        title: "Golden Hour Session",
+        title: "Die letzten Sonnenstrahlen und jede Menge neue Lines.",
         image: "asset/galerie1.jpg"
     },
     {
-        title: "FPV Moment",
+        title: "Wo andere Hindernisse sehen, sehe ich Möglichkeiten.",
         image: "asset/galerie2.jpg"
     },
     {
-        title: "Sunset Flight",
+        title: "Alles ausgeblendet. Nur die Drohne und der Moment.",
         image: "asset/galerie3.jpg"
     }
 ];
