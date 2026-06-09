f717d428d68bfe4b33c9679b0147c612544f12b7
tillyfpv <weickm@email.de>
2026-06-09 10:39:02 +0200

    Add files via upload

--- data/gallery.js
+++ data/gallery.js
@@ -1,14 +1,17 @@
 const galleryData = [
     {
-        title: "Die letzten Sonnenstrahlen und jede Menge neue Lines.",
+        title: "Golden Hour",
+        description: "Die letzten Sonnenstrahlen und neue Lines.",
         image: "asset/galerie1.jpg"
     },
     {
-        title: "Wo andere Hindernisse sehen, sehe ich Möglichkeiten.",
+        title: "Neue Perspektiven",
+        description: "Wo andere Hindernisse sehen, sehe ich Möglichkeiten.",
         image: "asset/galerie2.jpg"
     },
     {
-        title: "Alles ausgeblendet. Nur die Drohne und der Moment.",
+        title: "Im Flow",
+        description: "Alles ausgeblendet. Nur die Drohne und der Moment.",
         image: "asset/galerie3.jpg"
     }
 ];
